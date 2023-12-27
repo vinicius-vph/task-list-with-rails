c7edbbe7ee3f3a2d56d2eafcb1618e0a73f346c9
Vinicius Santos <vinicius.vph@gmail.com>
2023-12-27 23:37:07 +0000

    add new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
-  root to: 'home#welcome'
+  # namespace :auth do
+  #   resources :sessions, only: %i[new create destroy]
+  # end
+  root to: 'home#index'
+
+  resources :sessions, only: %i[new create destroy]
+
+  delete '/signout', to: 'sessions#destroy'
+  get '/signin', to: 'sessions#new'
+  get '/register', to: 'sessions#register'
+
   resources :tasks, only: %i[index new create edit update destroy] do
     post 'completions', to: 'completions#create'
     delete 'completions', to: 'completions#destroy'
   end
-  resources :sessions, only: %i[new create]
 end
